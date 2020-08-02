7653eb72e423acd050ad69c1b64c7381825d6b1e
Anna <git@anna.lgbt>
2020-08-01 20:39:25 -0400

    refactor: remove useless imports

--- Globetrotter/GlobetrotterPlugin.cs
+++ Globetrotter/GlobetrotterPlugin.cs
@@ -1,10 +1,6 @@
 ﻿using Dalamud.Game.Command;
 using Dalamud.Plugin;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Globetrotter {
     public class GlobetrotterPlugin : IDalamudPlugin {
--- Globetrotter/PluginUI.cs
+++ Globetrotter/PluginUI.cs
@@ -1,10 +1,6 @@
 ﻿using Dalamud.Plugin;
 using ImGuiNET;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Globetrotter {
     class PluginUI {
